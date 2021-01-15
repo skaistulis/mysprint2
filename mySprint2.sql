3d28c610f4e145a71752b26fa4ba7c29f27f37ee
skaistulis <68568456+skaistulis@users.noreply.github.com>
2021-01-15 18:10:58 +0200

    final
    final

--- mySprint2.sql
+++ mySprint2.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `mysprint2` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_lt_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `mysprint2`;
 -- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: mysprint2
@@ -75,4 +77,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-01-15 17:45:11
+-- Dump completed on 2021-01-15 18:10:11
